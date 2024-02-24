fde0befc9b0824eae85a00a91fb294dcaa8116a3
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2024-02-24 08:50:59 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximum Sum Problem/maximum-sum-problem.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.maxSum(n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int maxSum(int n) 
+    { 
+        if(n<=11)return n;
+            if(n==12)return 13;
+            int ans=0;
+            ans+=maxSum(n/2)+maxSum(n/3)+maxSum(n/4);
+            return ans;
+    } 
+}
