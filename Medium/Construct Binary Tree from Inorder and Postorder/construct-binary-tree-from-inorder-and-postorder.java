30f35329e8294b02d36eba1a9862f96f31fe47f4
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2024-05-04 16:14:29 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Construct Binary Tree from Inorder and Postorder/construct-binary-tree-from-inorder-and-postorder.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int value) {
+        data = value;
+        left = null;
+        right = null;
+    }
+}
+
+class InorderPostorderToTree {
+    public void preOrder(Node root) {
+        if (root == null) return;
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+
+        preOrder(root.right);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        InorderPostorderToTree ip = new InorderPostorderToTree();
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int inorder[] = new int[n];
+            int postorder[] = new int[n];
+            for (int i = 0; i < n; i++) inorder[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) postorder[i] = sc.nextInt();
+            GfG g = new GfG();
+            Node root = g.buildTree(inorder, postorder, n);
+            ip.preOrder(root);
+            System.out.println();
+
+            T--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+/* Tree node structure
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+        Node(int value)
+    {
+        data = value;
+        left = null;
+        right = null;
+    }
+}*/
+
+
+class GfG
+{
+    int p;
+    Node build(int in[],int post[],int start,int end)
+    {
+        if(start>end)
+        return null;
+        Node head=new Node(post[p--]);
+        int i=start;
+        for(;i<=end;i++)
+        {
+            if(in[i]==head.data)
+            break;
+        }
+        head.right=build(in,post,i+1,end);
+        head.left=build(in,post,start,i-1);
+        return head;
+    }
+    Node buildTree(int in[], int post[], int n) {
+        p=n-1;
+        return build(in,post,0,n-1);
+    }
+}
