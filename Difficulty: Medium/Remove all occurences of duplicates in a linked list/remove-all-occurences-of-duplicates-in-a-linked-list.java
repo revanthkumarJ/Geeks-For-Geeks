6038374f02277c4a9b381c0cf55060c608c67727
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2024-07-03 09:07:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove all occurences of duplicates in a linked list/remove-all-occurences-of-duplicates-in-a-linked-list.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class GFG {
+    Node head;
+    Node tail;
+
+    public void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Drier program to test above functions */
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int N = sc.nextInt();
+            GFG llist = new GFG();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < N; i++) {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+
+            Solution ob = new Solution();
+            head = ob.removeAllDuplicates(llist.head);
+            llist.printList(head);
+
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public Node removeAllDuplicates(Node head) {
+        
+        Node temp = head;
+        Node dummy = new Node(-1);
+        dummy.next=head;
+        Node k = dummy;
+        while(temp!=null){
+           if(temp.next!=null && temp.data==temp.next.data){
+               while(temp.next!=null && temp.data==temp.next.data){
+                   temp=temp.next;
+               }
+               k.next=temp.next;
+           }else{
+               k=k.next;
+           }
+           temp=temp.next;
+           
+        }
+        return dummy.next;
+    }
+}
